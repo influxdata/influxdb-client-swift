a02d87aff849f1d006a9f06c0e241af207c1680a
Jakub Bednar <jakub.bednar@gmail.com>
2020-12-11 11:15:25 +0100

    chore: added junit reporter

--- Package.swift
+++ Package.swift
@@ -13,13 +13,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
          .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift", from: "5.1.1"),
          .package(name: "CSV.swift", url: "https://github.com/yaslab/CSV.swift", from: "2.4.2"),
+         .package(name: "SwiftTestReporter", url: "https://github.com/allegro/swift-junit.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "InfluxDBSwift", dependencies: ["Gzip", .product(name: "CSV", package: "CSV.swift")]),
         .target(name: "InfluxDBSwiftApis", dependencies: ["InfluxDBSwift"]),
-        .testTarget(name: "InfluxDBSwiftTests", dependencies: ["InfluxDBSwift"]),
-        .testTarget(name: "InfluxDBSwiftApisTests", dependencies: ["InfluxDBSwiftApis"]),
+        .testTarget(name: "InfluxDBSwiftTests", dependencies: ["InfluxDBSwift", "SwiftTestReporter"]),
+        .testTarget(name: "InfluxDBSwiftApisTests", dependencies: ["InfluxDBSwiftApis", "SwiftTestReporter"]),
     ]
 )
---
+++ Tests/InfluxDBSwiftTests/AAJunitReportInitializerTests.swift
@@ -0,0 +1,19 @@
+//
+// Created by Jakub Bednář on 11/12/2020.
+//
+
+import Foundation
+
+@testable import InfluxDBSwift
+import XCTest
+import SwiftTestReporter
+
+class AAJunitReportInitializerTest: XCTestCase {
+    override class func setUp() {
+        _ = TestObserver()
+        super.setUp()
+    }
+
+    // https://github.com/allegro/swift-junit/issues/12#issuecomment-725264315
+    func testInit() {}
+}
