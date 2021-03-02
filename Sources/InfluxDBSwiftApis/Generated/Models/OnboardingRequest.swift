44a199c8efd3ae855d5926ace9d4522dec014bf5
Jakub Bednar <jakub.bednar@gmail.com>
2021-03-02 07:42:56 +0100

    chore: updated API

--- Sources/InfluxDBSwiftApis/Generated/Models/OnboardingRequest.swift
+++ Sources/InfluxDBSwiftApis/Generated/Models/OnboardingRequest.swift
@@ -14,13 +14,18 @@ public struct OnboardingRequest: Codable {
     public var password: String?
     public var org: String
     public var bucket: String
+    public var retentionPeriodSeconds: Int?
+    /** Retention period *in nanoseconds* for the new bucket. This key&#39;s name has been misleading since OSS 2.0 GA, please transition to use &#x60;retentionPeriodSeconds&#x60;  */
+    
+    @available(*, deprecated, message: "This property is deprecated.")
     public var retentionPeriodHrs: Int?
 
-    public init(username: String, password: String? = nil, org: String, bucket: String, retentionPeriodHrs: Int? = nil) {
+    public init(username: String, password: String? = nil, org: String, bucket: String, retentionPeriodSeconds: Int? = nil, retentionPeriodHrs: Int? = nil) {
         self.username = username
         self.password = password
         self.org = org
         self.bucket = bucket
+        self.retentionPeriodSeconds = retentionPeriodSeconds
         self.retentionPeriodHrs = retentionPeriodHrs
     }
 
