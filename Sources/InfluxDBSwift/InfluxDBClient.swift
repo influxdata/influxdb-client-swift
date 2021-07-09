b313d31788affd8afe603a9f03c1b3a646403bb7
Jakub Bednar <jakub.bednar@gmail.com>
2021-07-09 09:01:20 +0200

    chore(release): prepare for next development iteration

--- Sources/InfluxDBSwift/InfluxDBClient.swift
+++ Sources/InfluxDBSwift/InfluxDBClient.swift
@@ -24,7 +24,7 @@ import FoundationNetworking
 /// ````
 public class InfluxDBClient {
     /// Version of client.
-    public static var version: String = "0.6.0"
+    public static var version: String = "0.7.0dev"
     /// InfluxDB host and port.
     public let url: String
     /// Authentication token.
